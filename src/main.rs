bdfb239adbecaeb89cc127288be8f75b1d4484fb
Etienne Batise <etienne.batise@gmail.com>
2020-06-02 23:35:51 +0200

    Add graceful shutdown to server

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,13 @@ async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, hyper::Error
     Ok(res)
 }
 
+async fn shutdown_signal() {
+    // Wait for the CTRL+C signal
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
+}
+
 #[tokio::main]
 async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 4000));
@@ -21,8 +28,12 @@ async fn main() {
         Ok::<_, hyper::Error>(service_fn(hello_world))
     });
     let server = Server::bind(&addr).serve(make_svc);
+
+    // And now add a graceful shutdown signal...
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
+
     // Run this server for... forever!
-    if let Err(e) = server.await {
+    if let Err(e) = graceful.await {
         eprintln!("server error: {}", e);
     }
 }
