ea47620c9812a3e11bb726c9a39a0c08b572411e
Etienne Batise <etienne.batise@gmail.com>
2020-06-07 21:59:19 +0200

    Proxy the request instead of solely « getting » the uri

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use hyper::{Body, Request, Response, Server, Client, Uri};
 use hyper::service::{make_service_fn, service_fn};
 use hyper::header::{ACCESS_CONTROL_ALLOW_ORIGIN, HeaderValue};
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    let uri_str = format!("{}", _req.uri());
+async fn hello_world(mut req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let uri_str = format!("{}", req.uri());
     let url_str = format!("http://{}", &uri_str[1..]);
-    // FIXME : Fails silently if url url_str cannot be parsed
     let url = url_str.parse::<Uri>().expect("failed to parse URL");
-    let mut res = Client::new().get(url).await?;
+    // ^ Generate thread panic when uri is empty
+    *req.uri_mut() = url;
+    let mut res = Client::new().request(req).await?;
     let headers = res.headers_mut();
     headers.insert(ACCESS_CONTROL_ALLOW_ORIGIN, HeaderValue::from_static("*"));
     Ok(res)
