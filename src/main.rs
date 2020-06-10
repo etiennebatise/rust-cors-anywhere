2fa68bebe49932f602abc049793bdeacedb15351
Etienne Batise <etienne.batise@gmail.com>
2020-06-10 23:57:01 +0200

    Add CLI parsing for host and port

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server, Client, Uri};
+use hyper::header::{HeaderValue, ACCESS_CONTROL_ALLOW_ORIGIN, HOST};
 use hyper::service::{make_service_fn, service_fn};
-use hyper::header::{HOST, ACCESS_CONTROL_ALLOW_ORIGIN, HeaderValue};
+use hyper::{Body, Client, Request, Response, Server, Uri};
+use std::net::{Ipv4Addr, SocketAddr};
+use structopt::StructOpt;
+// use failure::ResultExt;
+// use exitfailure::ExitFailure;
 
 fn debug_request<T>(req: &Request<T>) -> () {
     println!("{}", req.method());
@@ -11,14 +14,15 @@ fn debug_request<T>(req: &Request<T>) -> () {
     }
 }
 
-async fn hello_world(mut req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    let uri : &str = &req.uri().to_string()[1..];
+async fn proxy(mut req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let uri: &str = &req.uri().to_string()[1..];
     // ^ TODO Dont trust user input ([1..] might fail)
     let target_url = format!("http://{}", uri);
-    let cols : Vec<&str> = uri.splitn(2, '/').collect::<Vec<&str>>();
+    let cols: Vec<&str> = uri.splitn(2, '/').collect::<Vec<&str>>();
     let host = cols[0];
     // ^ TODO  Don't trust user input(uri might not contain '/')
-    req.headers_mut().insert(HOST, HeaderValue::from_str(host).unwrap());
+    req.headers_mut()
+        .insert(HOST, HeaderValue::from_str(host).unwrap());
     *req.uri_mut() = target_url.parse::<Uri>().expect("failed to parse URL");
     // ^ TODO generate tokio crash when target_url is not a valid uri
     let mut res = Client::new().request(req).await?;
@@ -34,12 +38,19 @@ async fn shutdown_signal() {
         .expect("failed to install CTRL+C signal handler");
 }
 
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(long = "host")]
+    host: Ipv4Addr,
+    #[structopt(short = "p", long = "port")]
+    port: u16,
+}
+
 #[tokio::main]
 async fn main() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 4000));
-    let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, hyper::Error>(service_fn(hello_world))
-    });
+    let args = Cli::from_args();
+    let addr = SocketAddr::from((args.host, args.port));
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, hyper::Error>(service_fn(proxy)) });
     let server = Server::bind(&addr).serve(make_svc);
 
     // And now add a graceful shutdown signal...
